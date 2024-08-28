3ca291ff95b84a0cf32a209d22c827f92724532e
Simon Suo <simonsdsuo@gmail.com>
2024-08-28 16:00:52 -0700

    wip

--- llama_parse/base.py
+++ llama_parse/base.py
@@ -137,6 +137,10 @@ class LlamaParse(BasePydanticReader):
         default=None,
         description="The model name for the vendor multimodal API.",
     )
+    take_screenshot: bool = Field(
+        default=False,
+        description="Whether to take a screenshot of the document.",
+    )
 
     @field_validator("api_key", mode="before", check_fields=True)
     @classmethod
@@ -206,6 +210,7 @@ class LlamaParse(BasePydanticReader):
             "vendor_multimodal_api_key": self.vendor_multimodal_api_key,
             "use_vendor_multimodal_model": self.use_vendor_multimodal_model,
             "vendor_multimodal_model_name": self.vendor_multimodal_model_name,
+            "take_screenshot": self.take_screenshot,
         }
 
         # only send page separator to server if it is not None
